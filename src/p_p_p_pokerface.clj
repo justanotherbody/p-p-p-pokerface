983f9e3ac77d5bf630739aa223f1ca4d2d4dfe0c
Mike Richards <justanotherbody@gmail.com>
2014-07-13 14:59:22 -0700

    Completed poker methods

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[encoded-rank _] card]
+  (if (Character/isDigit encoded-rank)
+    (Integer/valueOf (str encoded-rank))
+    (let [ranks {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+      (get ranks encoded-rank)))))
 
 (defn suit [card]
-  nil)
+  (let [[_, suit] card]
+    (str suit)))
+
+(defn rank-counts [hand]
+  (frequencies (map rank hand)))
+
+(defn has-n-of-a-kind [n hand]
+    (>= (apply max (vals (rank-counts hand))) n))
 
 (defn pair? [hand]
-  nil)
+    (has-n-of-a-kind 2 hand))
+
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-n-of-a-kind 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-n-of-a-kind 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (set (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (= `(2, 3) (sort (vec (vals (rank-counts hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [gte2? (fn [x] (>= x 2))
+        pair-or-better (filter gte2? (vals (rank-counts hand)))]
+    (gte2? (count pair-or-better))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-ranks (sort (map rank hand))
+        low-rank (first sorted-ranks)]
+    (or
+     (= sorted-ranks (range low-rank (+ low-rank 5)))
+     ; A is both 1 and 14, special-case the low ace straight
+     (= sorted-ranks '(2 3 4 5 14)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+   (straight? hand)
+   (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checker-handvalue [
+                           [pair? 1]
+                           [two-pairs? 2]
+                           [three-of-a-kind? 3]
+                           [straight? 4]
+                           [flush? 5]
+                           [full-house? 6]
+                           [four-of-a-kind? 7]
+                           [straight-flush? 8]]
+        valid-hand-values (map (fn [[checker?, value]] (if (checker? hand) value 0)) checker-handvalue)]
+    (apply max valid-hand-values)))
